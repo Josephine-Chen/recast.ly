ae4a80471fd9b2ee253b82903510f39806d3c52e
Josephine Chen <jychen@g.hmc.edu>
2017-01-07 10:28:55 -0800

    searchYouTube tests passed

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var searchParams = {
+    type: 'video',
+    part: 'snippet',
+    q: options.query,
+    maxResults: options.max,
+    key: options.key,
+    videoEmbeddable: true
+  }
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: searchParams,
+    success: function(data) {
+      console.log(data.items);
+      callback(data.items);
+    },
+    error: function(data) {
+      console.log('Error D:');
+    }
+  });
+
 };
 
 window.searchYouTube = searchYouTube;
