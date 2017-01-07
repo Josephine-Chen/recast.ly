161a559b4bac9d5527977f946b87394736178e0c
Josephine Chen <jychen@g.hmc.edu>
2017-01-06 19:40:45 -0800

    Video player tests passed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,27 @@
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
-  }
+    this.state = {
+      videos: {exampleVideoData},
+      playerVid: {exampleVideoData[0]}
+    }
+    }
 
   render() {
     return (
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer />
+          <VideoPlayer video={this.state.playerVid}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={exampleVideoData}/>
+          <VideoList videos={this.state.videos}/>
         </div>
       </div>
-      )
+    );
   }
 }
-
-// var App = () => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer />
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList videos={exampleVideoData}/>
-//     </div>
-//   </div>
-// );
+//<VideoPlayer videos={exampleVideoData}/>
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
