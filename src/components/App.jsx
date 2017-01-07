6edf5828ace87ef5ccda7b5841ccc4595e742eec
Josephine Chen <jychen@g.hmc.edu>
2017-01-06 22:39:12 -0800

    App tests work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: {exampleVideoData},
-      playerVid: {exampleVideoData[0]}
-    }
-    }
+      vids: {exampleVideoData},
+      currentVid: {
+        id: {
+          videoId: '',
+        },
+        snippet: {
+          title: '',
+          description: '',
+          },
+      }
+    };
+  }
+  update(newVid) {
+    this.setState({
+      currentVid: newVid
+    });
+  }
 
   render() {
     return (
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer video={this.state.playerVid}/>
+          <VideoPlayer video={this.state.currentVid}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={this.state.videos}/>
+          <VideoList onEntryTitleClicked={this.update.bind(this)} videos={exampleVideoData}/>
         </div>
       </div>
     );
   }
 }
-//<VideoPlayer videos={exampleVideoData}/>
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
